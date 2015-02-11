f838aea20fb0b6b37891f1ec83d76ac5bd7475ec
shuaib88 <shuaibahmed@wireless-208-247.uchicago.edu>
2015-02-10 20:09:43 -0600

    Another round

--- movie_database.py
+++ movie_database.py
@@ -4,7 +4,19 @@ class Movie:
 	pass
 
 	def find_highest_rated(self):
-		pass
+		with open("imdb_top250.csv", "r", encoding="utf-8") as csvFile:
+			movieReader = csv.reader(csvFile, delimiter=",")
+
+			movieList = []
+
+			for row in movieReader:
+				movie = Movie()
+				movie.title = row[1]
+				movie.imdb_rating = row[3]
+				movie.director = row[2]
+				#movie.plot = have to access the API using the url
+				movieList.append(movie)
+
 
 	def title(self):
 		pass
@@ -18,22 +30,12 @@ class Movie:
 	def plot(self):
 		pass
 
-def read_movie_data():
-	with open("imdb_top250.csv", "r", encoding="utf-8") as csvFile:
-		movieReader = csv.reader(csvFile, delimiter=",")
-
-		movieList = []
-
-		for row in movieReader:
-			movie = Movie()
-			movie.title = 
-
 
 ##had trouble doing a few things opening the csv file and creating objects
 ##also had trouble just getting the best_movie to pass assertion even though i created a Movie class
 ##I know i create a for loop that loopst through the movie file
-I can use a similar method to the divvy problem to keep track of the highest rating
-then I can 
+# I can use a similar method to the divvy problem to keep track of the highest rating
+# then I can 
 
 
 # best_movie = Movie.find_highest_rated('imdb_top250.csv') 

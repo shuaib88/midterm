72de04df6841551fbaf68a10b444866e3f0be84c
shuaib88 <shuaibahmed@wireless-208-247.uchicago.edu>
2015-02-10 19:44:29 -0600

    trying to build an object

--- movie_database.py
+++ movie_database.py
@@ -6,5 +6,24 @@ class Movie:
 	def find_highest_rated(self, file):
 		pass
 
+def read_movie_data():
+	with open("imdb_top250.csv", "r", encoding="utf-8") as csvFile:
+		movieReader = csv.reader(csvFile, delimiter=",")
+
+		movieList = []
+
+		for row in movieReader:
+			movie = Movie()
+			movie.imdbID = row[0]
+			movieList.append(movie)
+
+	return movieList
+
+read_movie_data()
+movieList
+
+
+# best_movie = Movie.find_highest_rated('imdb_top250.csv') 
+
 
 

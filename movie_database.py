7e8eeb14667be3a709a3ec82bd24448a29d07c02
shuaib88 <shuaibahmed@wireless-208-247.uchicago.edu>
2015-02-10 19:52:20 -0600

    more tweaking

--- movie_database.py
+++ movie_database.py
@@ -3,24 +3,8 @@ import csv
 class Movie:
 	pass
 
-	def find_highest_rated(self, file):
+	def find_highest_rated(moviefile):
 		pass
-
-def read_movie_data():
-	with open("imdb_top250.csv", "r", encoding="utf-8") as csvFile:
-		movieReader = csv.reader(csvFile, delimiter=",")
-
-		movieList = []
-
-		for row in movieReader:
-			movie = Movie()
-			movie.imdbID = row[0]
-			movieList.append(movie)
-
-	return movieList
-
-read_movie_data()
-movieList
 
 
 # best_movie = Movie.find_highest_rated('imdb_top250.csv') 

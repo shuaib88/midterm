91cb8dc7ff91c5919c58220542db634f1ead0fbc
shuaib88 <shuaibahmed@wireless-208-247.uchicago.edu>
2015-02-10 20:15:54 -0600

    last

--- movie_database.py
+++ movie_database.py
@@ -24,11 +24,8 @@ class Movie:
 # I can use a similar method to the divvy problem to keep track of the highest rating
 # I also need to loop through the csv file
 # and build a new object with attributes for title, imdb_rating, director, plot etc... 
-#for the plot 
+#for the plot, I need to read the url to get the data i need
 
-
-
-# best_movie = Movie.find_highest_rated('imdb_top250.csv') 
 
 
 
